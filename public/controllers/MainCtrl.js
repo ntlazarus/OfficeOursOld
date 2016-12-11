89e0335e043080167c38ae7f8579718d6c2431f2
Nikhil Rabindra <nikhil.rabindra@gmail.com>
2016-12-11 17:49:09 -0500

    added mew html and javascript

--- public/controllers/MainCtrl.js
+++ public/controllers/MainCtrl.js
@@ -5,7 +5,11 @@ app.controller("MainCtrl", function($scope) {
   
   $scope.users = [{"name": "Nahum", "city": "Atlanta", "college": "Tech"},
                  {"name": "Addison", "city": "Ninety Six", "college": "Tech"},
-                 {"name": "Nikhil", "city": "Bangalore", "college": "Tech"}]
+                 {"name": "Nikhil", "city": "Bangalore", "college": "Tech"}];
+
+
+
+  $scope.tips = ['Tip 1', 'Tip 2', "Tip 3", 'Tip 4', 'Tip 5', "Tip 6", 'Tip 7', 'Tip 8', "Tip 9"];
     
   
   $scope.login = function(){
